fff67886b50d6cdcbd5b3f1ddb1a00e93a0b26db
Architect <ng_sou@163.com>
2021-05-07 21:31:53 +0800

    阶乘, 阶æ上台阶问题

--- Algorithm/factorial.go
+++ Algorithm/factorial.go
@@ -7,7 +7,20 @@ func factorial(n uint64) uint64 {
 		return 1
 	}
 
-	return n * factorial(n - 1)
+	return n * factorial(n-1)
+}
+
+// 上台阶问题
+func upStep(n uint64) uint64 {
+	if n == 1 {
+		// 只有一个台阶
+		return 1
+	} else if n == 2 {
+		// 只有两个台阶
+		return 2
+	}
+
+	return upStep(n-1) + upStep(n-2)
 }
 
 func main() {
