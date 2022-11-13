868db3a745d3ff133c315397c1f211294052c771
niujunqing <niujunqing@angelalign.com>
2022-11-13 20:24:40 +0800

    CRM_Tools

--- CrmTools/CrmTools.go
+++ CrmTools/CrmTools.go
@@ -24,8 +24,19 @@ type user struct {
 	userName string
 }
 
-type taskInfo struct {
-	taskId string
+type stageLastModel struct {
+	StageId      string
+	CaseId       string
+	MaterialType int
+	ReceiptId    string
+	Qualified    int
+}
+
+type caseInfo struct {
+	CaseId       string
+	CaseCode     string
+	TeenagerInfo int
+	CaseState    string
 }
 
 func apiIndex(w http.ResponseWriter, r *http.Request) {
@@ -58,13 +69,92 @@ func getUserInfoById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getCaseInfo(w http.ResponseWriter, r *http.Request) {
+	// 初始化数据库连接
+	err := initDb()
+	if err != nil {
+		// 数据库初始化失败
+		_, err := fmt.Fprintf(w, "数据库初始化失败, err: %v", err)
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	caseIdStr := r.FormValue("caseId")
+
+	var caseInfo caseInfo
+	caseInfo = getCaseInfoByCaseId(caseIdStr)
+
+	_, fErr := fmt.Fprintf(w, "{\"caseId\":\"%v\", \"CaseCode\":\"%v\", \"TeenagerInfo\":\"%v\", \"TeenagerInfoCn\":\"%v\", \"CaseState\":\"%v\", \"CaseStateCn\":\"%v\"}", caseInfo.CaseId, caseInfo.CaseCode, caseInfo.TeenagerInfo, caseUseA6OrAngelPole(caseInfo.TeenagerInfo), caseInfo.CaseState, caseStateMapping(caseInfo.CaseState))
+	if fErr != nil {
+		return
+	}
+}
+
+func getCaseInfoByCaseId(caseId string) caseInfo {
+	caseIdSql := "SELECT " +
+		"ecc.id_c,ec.name,ecc.is_teenager_c,ecc.state_c " +
+		"FROM ea_case ec " +
+		"INNER JOIN ea_case_cstm ecc ON ecc.id_c = ec.id " +
+		"WHERE ecc.id_c = ? "
+
+	var caseInfo caseInfo
+	err := dbObj.QueryRow(caseIdSql, caseId).Scan(&caseInfo.CaseId, &caseInfo.CaseCode, &caseInfo.TeenagerInfo, &caseInfo.CaseState)
+	if err != nil {
+		fmt.Println("查询出错：", err)
+		return caseInfo
+	}
+
+	return caseInfo
+}
+
+func caseStateMapping(caseState string) (caseStateStr string) {
+	/**
+	#ecc.state_c 病例状态 1:资料处理中 4:3D设计中 5:3D设计待确认 6:加工中 7:已发货 8:暂停
+	#                   9:结束 10:不收治 12:3D设计已确认 20:目标位设计中 21:目标位待确认
+	#                   22:目标位已确认 30:产品待确认 31:产品已确认
+	*/
+	var caseStateMap = make(map[string]string, 10)
+	caseStateMap["^1^"] = "资料处理中"
+	caseStateMap["^4^"] = "3D设计中"
+	caseStateMap["^5^"] = "3D设计待确认"
+	caseStateMap["^6^"] = "加工中"
+	caseStateMap["^7^"] = "已发货"
+	caseStateMap["^8^"] = "暂停"
+	caseStateMap["^9^"] = "结束"
+	caseStateMap["^12^"] = "3D设计已确认"
+
+	value, exist := caseStateMap[caseState]
+	if exist {
+		return value
+	}
+
+	return "未知状态"
+}
+
+func caseUseA6OrAngelPole(teenager int) (caseTeenager string) {
+	var caseTeenagerMap = make(map[int]string, 3)
+
+	caseTeenagerMap[0] = " "
+	caseTeenagerMap[1] = "A6病例"
+	caseTeenagerMap[2] = "下颌前导"
+
+	value, exist := caseTeenagerMap[teenager]
+	if exist {
+		return value
+	}
+
+	return "不存在"
+}
+
 // 初始化数据库连接
 func initDb() (err error) {
 	// 数据库连接
 	if env == "local" {
 		dsn = "root:@tcp(127.0.0.1:3306)/tools"
 	} else if env == "dev" {
-		dsn = "crm_dev:YKdYXQfhjjzDr281@tcp(rm-uf67h6texeyf9f325fo.mysql.rds.aliyuncs.com:3306)/crm_dev"
+		dsn = "crm_dev:YKdYXQfhjjzDr281@tcp(rm-uf67h6texeyf9f325.mysql.rds.aliyuncs.com:3306)/crm_dev"
 	}
 
 	// open
@@ -154,13 +244,14 @@ func newUser(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// 设置当前环境
-	env = "local"
+	env = "dev"
 
 	http.HandleFunc("/", apiIndex)
 	http.HandleFunc("/getUserInfo", getUserInfoById)
 	http.HandleFunc("/newUser", newUser)
+	http.HandleFunc("/getCaseInfo", getCaseInfo)
 
-	httpErr := http.ListenAndServe(":80", nil)
+	httpErr := http.ListenAndServe(":8090", nil)
 	if httpErr != nil {
 		fmt.Println("http server start err: ", httpErr)
 		return
